2af284c1fbe56472c7c20334618fa9dec7731c22
Yan Tavares <yantavares2002@gmail.com>
2023-03-15 14:39:45 -0300

    Resultados múltiplos

--- Estudos_linguagem_go/Basico/importacao.go
+++ Estudos_linguagem_go/Basico/importacao.go
@@ -1,10 +1,12 @@
 package main
 
+// Este é um programa básico em Go que importa os pacotes "fmt" e "math".
 import (
 	"fmt"
 	"math"
 )
 
+// Imprime na tela o resultado da raiz quadrada de 7 usando o pacote math
 func main() {
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 }
---
+++ Estudos_linguagem_go/Basico/resultados_multiplos.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func main() {
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+}
+
+/*
+	Resultados Múltiplos
+Uma função pode retornar qualquer número de resultados.
+
+A função swap retorna duas strings.
+*/
