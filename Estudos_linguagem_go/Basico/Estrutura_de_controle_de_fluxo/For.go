288bffe084e168d320be62740abe0ce6e14a571c
Yan Tavares <yantavares2002@gmail.com>
2023-03-16 10:54:32 -0300

    Laço de repetição for

--- Estudos_linguagem_go/Basico/Estrutura_de_controle_de_fluxo/For.go
+++ Estudos_linguagem_go/Basico/Estrutura_de_controle_de_fluxo/For.go
@@ -9,3 +9,20 @@ func main() {
 	}
 	fmt.Println(sum)
 }
+
+/*
+
+For
+Go tem apenas uma estrutura de laço, o for.
+
+O laço for básico tem três componentes separados por ponto e vírgula:
+
+a instrução inicial: executada antes da primeira iteração
+a expressão de condição: avaliada antes de cada iteração
+a pós-instrução: executado no final de cada iteração
+A instrução inicial será freqüentemente uma declaração de variável curta, e variáveis declaradas são visíveis apenas no escopo da declaração do for.
+
+O loop irá parar de iterar quando a condição booleana for avaliada como 'false'.
+
+Nota: Ao contrário de outras linguagens como C, Java ou JavaScript, não há parênteses rodeando os três componentes da declaração for e as chaves { } são sempre necessárias.
+*/
