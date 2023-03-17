8821964fb21e5e0358320efa81e3aba2e5dc0c65
Yan Tavares <yantavares2002@gmail.com>
2023-03-17 12:14:11 -0300

    Padrões de um slice

---
+++ Estudos_linguagem_go/Basico/Structs,slices_e_maps/padroes_de_uma_slice.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{2, 3, 8, 9, 6, 5, 4}
+
+	s = s[1:5]
+	fmt.Println(s)
+
+	s = s[:2]
+	fmt.Println(s)
+
+	s = s[1:]
+	fmt.Println(s)
+}
+
+/*
+Padrões de uma Slice
+Ao "fatiar", você pode omitir as posições de limite altas ou baixas para usar seus padrões em seu lugar.
+
+O padrão é igual a zero para o limite baixo e o comprimento da slice para o limite alto.
+
+Para a matriz
+
+var a [10]int
+essas expressões das slices são equivalentes:
+
+a[0:10]
+a[:10]
+a[0:]
+a[:]
+*/
