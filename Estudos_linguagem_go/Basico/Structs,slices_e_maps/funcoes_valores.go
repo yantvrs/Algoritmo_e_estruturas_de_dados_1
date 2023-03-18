83c65cee342a16dbcd0637505a29d0f62ae4009e
Yan Tavares <yantavares2002@gmail.com>
2023-03-18 16:43:35 -0300

    Funções valores

---
+++ Estudos_linguagem_go/Basico/Structs,slices_e_maps/funcoes_valores.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
+}
+
+func main() {
+	hypot := func(x, y float64) float64 {
+		return math.Sqrt(x*x + y*y)
+	}
+
+	fmt.Println(hypot(5, 12))
+
+	fmt.Println(compute(hypot))
+	fmt.Println(compute(math.Pow))
+}
+
+/*
+	Funções valores
+Funções são valores também. Elas podem ser passadas assim como outros valores.
+
+Funções valores podem ser usadas como argumentos de funções e retornar valores.
+*/
