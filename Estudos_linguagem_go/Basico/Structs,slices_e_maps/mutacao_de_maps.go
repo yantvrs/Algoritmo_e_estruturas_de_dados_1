a55d992ce0dc43f43b35b73e2ae39721a253e068
Yan Tavares <yantavares2002@gmail.com>
2023-03-18 16:36:42 -0300

    Mutação de maps

--- Estudos_linguagem_go/Basico/Structs,slices_e_maps/mutacao_de_maps.go
+++ Estudos_linguagem_go/Basico/Structs,slices_e_maps/mutacao_de_maps.go
@@ -1,7 +1,43 @@
 package main
 
+import "fmt"
+
 func main() {
 	m := make(map[string]int)
 
 	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
+
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
+
+	delete(m, "Answer")
+	fmt.Println("The value:", m["Answer"])
+
+	v, ok := m["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
 }
+
+/*
+
+Mutação de Maps
+Inserir ou atualizar um elemento no map m:
+
+m[key] = elem
+Recuperar um elemento:
+
+elem = m[key]
+Excluir um elemento:
+
+delete(m, key)
+Teste que uma chave está presente com dois valores:
+
+elem, ok = m[key]
+Se key está em m, ok é true. Se não, ok é false.
+
+Se key não está no map então elem tem valor zero para o elemento do tipo do map.
+
+Nota: Se elem ou ok não foi declarado ainda você pode usar esta forma de declaração curta:
+
+elem, ok := m[key]
+*/
