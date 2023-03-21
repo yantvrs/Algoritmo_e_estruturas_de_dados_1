aefe5e042713a7da628316661b088a318f81715a
Yan Tavares <yantavares2002@gmail.com>
2023-03-21 13:27:39 -0300

    Refazendo a main

--- Unidade_1/Listas/arrayList.go
+++ Unidade_1/Listas/arrayList.go
@@ -1,18 +1,16 @@
 package main
 
-import "fmt"
-
 type ArrayList struct {
-	items []int
-	cap   int
+	items []int // array que armazena os elementos da lista
+	cap   int   // capacidade da lista
 }
 
-// Inicia a execução de inicialização
+// Inicializa a lista com uma capacidade inicial de 10 elementos
 func (list *ArrayList) Init() {
 	list.items = make([]int, 10)
 }
 
-// Dobra o tamanho do arrayList
+// Dobra a capacidade da lista
 func (list *ArrayList) double() {
 	doubleItems := make([]int, len(list.items)*2)
 
@@ -23,45 +21,48 @@ func (list *ArrayList) double() {
 	list.items = doubleItems
 }
 
-// Adiciona um novo elemento à lista
+// Adiciona um novo elemento ao final da lista
 func (list *ArrayList) Add(newItem int) {
 
-	// Dobra a arrayList caso não haja espaço suficiente
+	// Dobra a capacidade da lista caso necessário
 	if list.cap == len(list.items) {
 		list.double()
 	}
 
+	// Adiciona o novo elemento na próxima posição livre
 	list.items[list.cap] = newItem
 	list.cap++
 }
 
-// Adiciona um novo elemento em uma posição específica
+// Adiciona um novo elemento em uma posição específica da lista
 func (list *ArrayList) AddOnIndex(newItem int, index int) {
 
-	// Dobra a arrayList caso não haja espaço suficiente
+	// Dobra a capacidade da lista caso necessário
 	if list.cap == len(list.items) {
 		list.double()
 	}
 
-	// Alocando os items em novas posições
+	// Realoca os elementos para abrir espaço para o novo elemento
 	for i := list.cap; i > index; i-- {
 		list.items[i] = list.items[i-1]
 	}
 
+	// Adiciona o novo elemento na posição especificada
 	list.items[index] = newItem
 	list.cap++
 }
 
-// Remove um elemento da arrayList
+// Remove o último elemento da lista
 func (list *ArrayList) Remove() {
 	if list.cap > 0 {
 		list.cap--
 	}
 }
 
-// Remove um elemento da arrayList em posição específica
+// Remove um elemento em uma posição específica da lista
 func (list *ArrayList) RemoveOnIndex(index int) {
 	if index >= 0 && index < list.cap {
+		// Realoca os elementos para remover o elemento na posição especificada
 		for i := index; i < list.cap; i++ {
 			list.items[i] = list.items[i+1]
 		}
@@ -69,46 +70,23 @@ func (list *ArrayList) RemoveOnIndex(index int) {
 	}
 }
 
-// Retorna o número de elementos na lista
+// Retorna o tamanho atual da lista
 func (list *ArrayList) Size() int {
-	return len(list.items)
+	return list.cap
 }
 
+// Retorna o elemento em uma posição específica da lista
 func (list *ArrayList) Get(index int) int {
 	if index >= 0 && index < list.cap {
 		return list.items[index]
 	}
-	return -1 //TODO: add error handling
+	return -1 //TODO: adicionar tratamento de erro
 }
 
+// Substitui o elemento em uma posição específica da lista pelo valor especificado
 func (list *ArrayList) Set(value, index int) {
 	if index >= 0 && index < list.cap {
 		list.items[index] = value
 	}
-	//else //TODO: add error handling
-}
-
-func main() {
-	// Cria uma nova lista vazia
-	list := ArrayList{}
-
-	// Adiciona alguns elementos à lista
-	list.Add(10)
-	list.Add(20)
-	list.Add(30)
-
-	// Imprime o tamanho da lista e seus elementos
-	fmt.Println("Tamanho da lista:", list.Size())
-	fmt.Println("Elementos da lista:", list.items)
-
-	// Remove o segundo elemento da lista
-	list.Remove(1)
-
-	// Imprime o tamanho da lista e seus elementos novamente
-	fmt.Println("Tamanho da lista após a remoção:", list.Size())
-	fmt.Println("Elementos da lista após a remoção:", list.items)
-
-	// Obtém e imprime o primeiro elemento da lista
-	first := list.Get(0)
-	fmt.Println("Primeiro elemento da lista:", first)
+	//TODO: adicionar tratamento de erro
 }
