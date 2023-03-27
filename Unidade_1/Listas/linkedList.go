0580b93f5c4db4a526ce31f7ecedd065d4694fa2
Yan Tavares <yantavares2002@gmail.com>
2023-03-27 14:32:53 -0300

    modificações das funções

--- Unidade_1/Listas/linkedList.go
+++ Unidade_1/Listas/linkedList.go
@@ -1,120 +1,34 @@
 package main
 
-import "fmt"
-
-// Definindo a estrutura do nó da lista encadeada
-type Node struct {
-	data int   // Valor do nó
-	next *Node // Próximo nó da lista
+// Estrutura que representa a linkedlist
+type linkedList struct {
+	head *node
+	size int
 }
 
-// Definindo a estrutura da lista encadeada
-type LinkedList struct {
-	head *Node // Nó inicial da lista
+// Estrutura que representa cada nó
+type node struct {
+	value int
+	next  *node
 }
 
-// Função para adicionar um nó no fim da lista
-func (list *LinkedList) Add(data int) {
-	newNode := &Node{data, nil} // Criando um novo nó
+// Adiciona elemento no final da lista
+func (list *linkedList) Add(value int) {
+	newNode := node{value, nil}
 
-	if list.head == nil { // Se a lista estiver vazia, o novo nó se torna o primeiro
-		list.head = newNode
+	assistant := linkedList.head
+	prev := assistant
+
+	for assistant != nil {
+		prev := assistant
+		assistant := assistant.next
+	}
+
+	if prev == nil {
+		linkedList.head = &newNode
 	} else {
-		current := list.head // Começando a busca pelo fim da lista
-
-		for current.next != nil { // Percorrendo a lista até encontrar o último nó
-			current = current.next
-		}
-
-		current.next = newNode // Adicionando o novo nó no fim da lista
-	}
-}
-
-// Função para remover um nó da lista
-func (list *LinkedList) Remove(data int) {
-	if list.head == nil { // Se a lista estiver vazia, não há nada para remover
-		return
+		prev.next := &newNode
 	}
 
-	if list.head.data == data { // Se o nó a ser removido for o primeiro da lista
-		list.head = list.head.next // O segundo nó se torna o primeiro
-		return
-	}
-
-	current := list.head // Começando a busca pelo nó a ser removido
-
-	for current.next != nil {
-		if current.next.data == data { // Se o próximo nó for o nó a ser removido
-			current.next = current.next.next // Removendo o nó da lista
-			return
-		}
-		current = current.next // Continuando a busca
-	}
-}
-
-// Função para obter o tamanho da lista
-func (list *LinkedList) Size() int {
-	size := 0 // Começando com tamanho 0
-
-	if list.head == nil { // Se a lista estiver vazia, retorna tamanho 0
-		return size
-	}
-
-	current := list.head // Começando a busca pelo fim da lista
-	size++               // Adicionando o primeiro nó
-
-	for current.next != nil { // Percorrendo a lista até encontrar o último nó
-		size++
-		current = current.next
-	}
-
-	return size
-}
-
-// Função para obter um nó a partir de um índice
-func (list *LinkedList) Get(index int) *Node {
-	if list.head == nil { // Se a lista estiver vazia, retorna nulo
-		return nil
-	}
-
-	current := list.head // Começando a busca pelo nó com o índice desejado
-	i := 0
-
-	for i < index && current.next != nil { // Percorrendo a lista até encontrar o nó com o índice desejado
-		i++
-		current = current.next
-	}
-
-	if i == index { // Se encontrou o nó com o índice desejado, retorna o nó
-		return current
-	}
-
-	return nil // Caso contrário, retorna nulo
-}
-
-// Função principal
-func main() {
-	// Criando uma lista encadeada vazia
-	list := &LinkedList{}
-
-	// Adicionando alguns nós na lista
-	list.Add(10)
-	list.Add(20)
-	list.Add(30)
-	list.Add(40)
-
-	// Imprimindo o tamanho da lista
-	fmt.Printf("Tamanho da lista: %d\n", list.Size())
-
-	// Imprimindo o valor do segundo nó
-	secondNode := list.Get(1)
-	if secondNode != nil {
-		fmt.Printf("Valor do segundo nó: %d\n", secondNode.data)
-	}
-
-	// Removendo o terceiro nó da lista
-	list.Remove(30)
-
-	// Imprimindo o tamanho da lista novamente
-	fmt.Printf("Tamanho da lista após remover um nó: %d\n", list.Size())
+	linkedList.size++
 }
