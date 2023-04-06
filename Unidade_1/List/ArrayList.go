fdc07f6b2fff56f8ccbd3bb30675c5a891d879eb
Yan Tavares <yantavares2002@gmail.com>
2023-04-06 20:08:41 -0300

    Treinando a criação de arraylist

---
+++ Unidade_1/List/ArrayList.go
@@ -0,0 +1,18 @@
+package main
+
+type ArrayList struct {
+	items []int //Array que armazena os elementos da lista
+	cap   int   //Capacidade da lista
+}
+
+// Inicialização da lista
+
+// Duplicação da capacidade
+
+// Adicionar elemento no final
+
+// Adicionar elemento em uma posição específica
+
+// Remover elemento no final
+
+// Remover elemento em uma posição específica
