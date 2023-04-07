f778c96990d6ac25e6663e8a4bdb25dfa905395b
Yan Tavares <yantavares2002@gmail.com>
2023-04-07 19:44:22 -0300

    arraylist

--- Unidade_1/List/ArrayList/ArrayList.go
+++ Unidade_1/List/ArrayList/ArrayList.go
@@ -1,12 +1,50 @@
+package main
 
+type ArrayList struct {
+	items []int // Array que armazena os elementos da lista
+	size  int   // Quantidade de elementos da lista
+}
 
 // Inicialização da lista
+func (list *ArrayList) Init() {
+	list.items = make([]int, 10)
+}
 
 // Duplicação da capacidade
+func (list *ArrayList) Double() {
+	doubleSize := make([]int, len(list.items)*2)
+
+	for i := 0; i < len(list.items); i++ {
+		doubleSize[i] = list.items[i]
+	}
+
+	list.items = doubleSize
+}
 
 // Adicionar elemento no final
+func (list *ArrayList) AddEnd(newItem int, index int) {
+
+	// Dobra o tamanho da lista caso seja necessário
+	if len(list.items) == list.size {
+		list.Double()
+	}
+
+	// Adiciona o novo item no final da lista
+	list.items[list.size] = newItem
+	list.size++
+}
 
 // Adicionar elemento em uma posição específica
+func (list *ArrayList) AddIndex(newItem int) {
+
+	// Dobra o tamanho da lista caso seja necessário
+	if len(list.items) == list.size {
+		list.Double()
+	}
+
+	// Reorganizando os valores
+
+}
 
 // Remover elemento no final
 
