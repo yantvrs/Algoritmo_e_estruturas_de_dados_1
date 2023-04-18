c64e5c25b59843a3e9c6b1a4917f894919922dbf
Yan Tavares <99984484+yantvrs@users.noreply.github.com>
2023-04-18 14:26:00 -0300

    Update BubbleSort.go

--- BubbleSort.go
+++ BubbleSort.go
@@ -2,26 +2,25 @@ package main
 
 import "fmt"
 
-
-
 func BubbleSort( v []int) {
     size := len(v)
     for j := 0; j < size - 1; j++ {
-        var change = false
+        change := false
         for i := 0; i < size - 1 -j; i++ {
             if v[i] > v[i+1] {
                 v[i], v[i+1] = v[i+1], v[i]
                 change = true
             }
         }
-        if change == true {
+        if change == false {
             break
         }
     }
 }
 
 func main() {
-    v = []int{6,9,8,7,4}
+    v := []int{6,9,8,7,4}
+    fmt.Println("Vetor desordenado: ", v)
     BubbleSort(v)
-    fmt.Print(v)
+    fmt.Println("Vetor ordenado: ", v)
 }
