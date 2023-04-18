facc2b8d2d8e9a0770055f9c569c868912f7db94
Yan Tavares <99984484+yantvrs@users.noreply.github.com>
2023-04-18 14:00:21 -0300

    Update BubbleSort.go

--- BubbleSort.go
+++ BubbleSort.go
@@ -1,1 +1,27 @@
+package main
 
+import "fmt"
+
+
+
+func BubbleSort( v []int) {
+    size := len(v)
+    for j := 0; j < size - 1; j++ {
+        var change = false
+        for i := 0; i < size - 1 -j; i++ {
+            if v[i] > v[i+1] {
+                v[i], v[i+1] = v[i+1], v[i]
+                change = true
+            }
+        }
+        if change != true {
+            return
+        }
+    }
+}
+
+func main() {
+    v = []int{6,9,8,7,4}
+    BubbleSort(v)
+    fmt.Print(v)
+}
