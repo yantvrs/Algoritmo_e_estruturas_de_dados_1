3ef6471d517d101dde7e1c4220f6520e98893f37
Yan Tavares <yantavares2002@gmail.com>
2023-05-13 13:42:36 -0300

    Opção a pronta

--- Unidade_2/Lista/questao_1.go
+++ Unidade_2/Lista/questao_1.go
@@ -6,7 +6,21 @@ import "fmt"
 
 // a)SelectionSort (in-place)
 func SelectionSort(v []int) {
+	size := len(v)
 
+	for i := 0 ; i < size - 1; i++ {
+		iSmaller := i
+
+		for j := i + 1; j < size ; j++ {
+			if v[j] < v[iSmaller] {
+				iSmaller = j
+			}
+		}
+
+		if iSmaller != i {
+			v[i],v[iSmaller]=v[iSmaller],v[i]
+		}
+	}
 }
 
 // b)BubbleSort (in-place)
@@ -35,5 +49,12 @@ func CoutingSort() {
 }
 
 func main() {
+	fmt.Println("Algoritmos de ordenação\nVetor desordenado:")
+	
+	array := []int{9, 2, 5, 6, 7, 8, 4, 1, 3}
+	fmt.Println("\nVetor desordenado:", array)
 
+	fmt.Println("\nSelectionSort:")
+	SelectionSort(array)
+	fmt.Println("Ordenado: ", array)
 }
