75f9e998044680f437f57015cd92de645bf4899c
Yan Tavares <yantavares2002@gmail.com>
2023-05-13 12:21:31 -0300

    Iniciando questão 1

---
+++ Unidade_2/Lista/questao_1.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+/*1) Programe na linguagem C/Go cada um dos seguintes algoritmos de ordenação:*/
+
+// a)SelectionSort (in-place)
+func SelectionSort(v []int) {
+
+}
+
+// b)BubbleSort (in-place)
+func BubbleSort() {
+
+}
+
+// c)InsertionSort (in-place)
+func InsertionSort() {
+
+}
+
+// d)MergeSort
+func MergeSort() {
+
+}
+
+// e)QuickSort(com randomização de pivô)
+func QuickSort() {
+
+}
+
+// f)CoutingSort
+func CoutingSort() {
+
+}
+
+func main() {
+
+}
