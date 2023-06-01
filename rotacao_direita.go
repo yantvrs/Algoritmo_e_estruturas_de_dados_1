02f57ab8d0e830ba34204e614c3be39238853e8d
Yan Tavares <99984484+yantvrs@users.noreply.github.com>
2023-06-01 14:05:05 -0300

    Create rotacao_direita.go

---
+++ rotacao_direita.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type BstNode struct {
+  left *BstNode
+  value int
+  right *BstNode
+}
+
+func (bstNode *BstNode) RotationRight() BstNode{
+  left := bstNode.left
+  bstNode.left = bstNode.left.right
+  left.right = bstNode
+  return left
+}
+
+func (bstNode *BstNode) RotationLeft() BstNode{
+  right := bstNode.right
+  bstNode.right = bstNode.right.left
+  right.left = bstNode
+  return right
+}
