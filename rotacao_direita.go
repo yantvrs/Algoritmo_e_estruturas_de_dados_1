278b6b1218ed794d61b3b81295faad9e30cdabed
Yan Tavares <99984484+yantvrs@users.noreply.github.com>
2023-06-06 13:29:59 -0300

    Update rotacao_direita.go

--- rotacao_direita.go
+++ rotacao_direita.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+/*
+ 
+  Fator de balanceamento é calculado através da diferença entre a altura da direita menos o da esquerda.
+  bf = hr - hl
+  
+  Desbalanceado: 
+  bf >= 2
+  bf <= - 2 
+  
+  Caso 1: Desbalanceado para a esquerda -> bf= -2 -> Rotaciona p/ direita
+*/
+
 type BstNode struct {
   left *BstNode
   value int
