64d9f7b19ca3c20545c0c139c0f69d163de97beb
Yan Tavares <yantavares2002@gmail.com>
2023-07-04 12:43:34 -0300

    Interface árvore

--- Unidade_3/avl/avl_implementation.go
+++ Unidade_3/avl/avl_implementation.go
@@ -1,15 +1,31 @@
 package main
 
-import "fmt"
+type ITree interface {
+	Add(value int) *BstNode
+	Search(value int) bool
+	Min() int
+	Max() int
+	PrintPre()
+	PrintIn()
+	PrintPos()
+	Height() int
+	Remove(value int) *BstNode
+	IsBst() bool
+	Size() int
+	RotRight() *BstNode
+	RotLeft() *BstNode
+	Rebalance() *BstNode
+	UpdateProperties()
+}
 
 type BstNode struct {
-	left *BstNode
-	value int 
-	height int 
-	bf int 
-	right *BstNode
+	left   *BstNode
+	value  int
+	height int
+	bf     int
+	right  *BstNode
 }
 
 func (bstNode *BstNode) RotRight() *BstNode {
-	
+
 }
